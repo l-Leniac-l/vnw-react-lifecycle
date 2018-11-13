654c1a0381f6ee5bfb5ac4a73c72c19491a605a2
Lenilson Nascimento <lenilson.100@gmail.com>
2018-11-13 13:34:15 -0200

    o/

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.characters = [];
+
+    this.state = {
+      characters: []
+    };
+
+    this.search = this.search.bind(this);
+  }
+
+  async componentDidMount() {
+    let result = await axios.get("http://hp-api.herokuapp.com/api/characters");
+
+    this.characters = result.data;
+
+    this.setState({
+      characters: this.characters
+    });
+  }
+
+  renderCharacters() {
+    return this.state.characters.map((item, index) => {
+      return <p key={index}>{item.name}</p>;
+    });
+  }
+
+  search(event) {
+    event.preventDefault();
+
+    let text = event.target.value;
+
+    if (text === "") {
+      this.setState({
+        characters: this.characters
+      });
+      return;
+    }
+
+    let results = this.characters.filter(item => {
+      return item.name.toLowerCase().includes(text.toLowerCase());
+    });
+
+    this.setState({
+      characters: results
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <input
+            type="text"
+            style={{ width: "500px", height: "50px", fontSize: "24px" }}
+            onChange={this.search}
+          />
+          {this.renderCharacters()}
         </header>
       </div>
     );
